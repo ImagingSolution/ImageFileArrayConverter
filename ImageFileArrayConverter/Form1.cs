fae2b0c7fe9226d362927dda00b67c2ac894e4d2
Akira <yasukawaa@yahoo.co.jp>
2019-01-17 22:31:00 +0900

    初版

--- ImageFileArrayConverter/Form1.cs
+++ ImageFileArrayConverter/Form1.cs
@@ -8,6 +8,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+using System.IO;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+// 下記を参照の追加すること
+// WindowsBase
+// PresentationCore
+
 namespace ImageFileArrayConverter
 {
     public partial class Form1 : Form
@@ -15,6 +24,34 @@ namespace ImageFileArrayConverter
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+
+
+        private void SaveImage(string filename, Array bmpData, int width, int height, BitmapEncoder encoder, System.Windows.Media.PixelFormat format, double dpi = 96)
+        {
+            int stride = ((width * format.BitsPerPixel + 31) / 32) * 4;
+
+            System.Windows.Media.Imaging.BitmapSource bs = System.Windows.Media.Imaging.BitmapSource.Create(
+                width,
+                height,
+                dpi,
+                dpi,
+                format,
+                null,
+                bmpData,
+                stride);
+
+
+            using (Stream stream = new FileStream(filename, FileMode.Create))
+            {
+                var bf = BitmapFrame.Create(bs);
+                encoder.Frames.Add(bf);
+                encoder.Save(stream);
+            }
+
+
         }
     }
 }
